d10b883ebfba825bbaa50fae2facb747eea14c8e
flloyd2017 <lfhansen2017@gmail.com>
2025-02-15 10:09:44 -0800

    Update GovProgramsV2.py

--- GovProgramsV2.py
+++ GovProgramsV2.py
@@ -172,7 +172,7 @@ if st.button("Submit Selection"):
         st.warning("🚨 Entry for that email has already been submitted.")
     else:
         if not name or not email:
-        st.error("Name and Email are required to submit your selection.")
+            st.error("Name and Email are required to submit your selection.")
     elif not selected_agencies:
         st.error("Please select at least one program to fund.")
         save_data(name, email, selected_agencies)
